42c63c5f717750a47c32f99e44c1faadf410fca7
Filip Skřiváček <filskri019@pslib.cz>
2023-04-19 23:05:32 +0200

    MegaNajetaAplikace

--- App.js
+++ App.js
@@ -1,61 +1,62 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
-import { useState, useEffect } from 'react';
-import * as Location from 'expo-location';
+import { Button, StyleSheet, TextInput, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
-
   const [location, setLocation] = useState();
-
-  
+  const [loading, setLoading] = useState();
 
   useEffect(() => {
     const getPermissions = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
+      if (status !== "granted") {
         console.log("Prosím povolte GPS");
         return;
-      }  
+      }
     };
     getPermissions();
   }, []);
 
   const geocode = async () => {
+    setLoading(true);
     let currentLocation = await Location.getCurrentPositionAsync({});
-      setLocation(currentLocation);
-      console.log(currentLocation);
+    setLocation(currentLocation);
+    console.log(currentLocation);
+    setLoading(false);
   };
 
   const reverseGeocode = async () => {
-
     const reverseGeocodedAddress = await Location.reverseGeocodeAsync({
       longitude: location.coords.longitude,
-      latitude: location.coords.latitude
+      latitude: location.coords.latitude,
     });
 
     console.log("Adresa:");
     console.log(reverseGeocodedAddress);
   };
 
-
   const caniwork = () => {
     const companyLatitude = 50.7264924111979;
     const companyLongitude = 15.460478079662755;
     const tolerance = 100;
-  
+
+    // DDM 50.7264924111979, 15.460478079662755
+    // Zeyerovka 50.775603022284635, 15.052113442651669
+
     const distance = getDistance(
       location.coords.latitude,
       location.coords.longitude,
       companyLatitude,
       companyLongitude
     );
-  
+
     if (distance <= tolerance) {
       console.log("Ano, můžeš");
     } else {
       console.log("Ne, nemůžeš");
     }
   };
-  
+
   const getDistance = (lat1, lon1, lat2, lon2) => {
     const radlat1 = (Math.PI * lat1) / 180;
     const radlat2 = (Math.PI * lat2) / 180;
@@ -73,21 +74,21 @@ export default function App() {
     return dist * 1609.344;
   };
 
-
   return (
     <View style={styles.container}>
-      <Button title="Zjistit souřadnice" onPress={geocode} />
-      <Button title="Zjistit adresu" onPress={reverseGeocode} />
-      <Button title="Můžu pracovat?" onPress={caniwork} />
-    </View>
+    {loading && <Text>Načítání...</Text>}
+    <Button title="Zjistit souřadnice" onPress={geocode} />
+    <Button title="Zjistit adresu" onPress={reverseGeocode} />
+    <Button title="Můžu pracovat?" onPress={caniwork} />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
